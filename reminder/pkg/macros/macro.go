d1d8b93825c5e6d0590b73634887f005fdf3d2d0
shixuan song <shixuansong@shixuandeMacBook-Pro.local>
2020-04-24 14:44:29 +0800

    canlu

--- reminder/pkg/macros/macro.go
+++ reminder/pkg/macros/macro.go
@@ -1,7 +1,7 @@
 package macros
 
 const (
-	SendMailSpec      = "0 22 * * FRI,Thu"
+	SendMailSpec      = "0 22 * * FRI"
 	SendMailSpecTest1 = "33 16 * * FRI,Thu"
 	MinProbability    = 0.25
 	MaxProbability    = 0.90
