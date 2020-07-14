e9a0d077537543abef9680ce59947adae1772c57
shixuan song <shixuansong@shixuandeMacBook-Pro.local>
2020-07-14 17:41:41 +0800

    kafka writer

---
+++ kafka/producer/highlevel.go
@@ -0,0 +1,35 @@
+package producer
+
+import (
+	"context"
+	"github.com/fatih/color"
+	"github.com/segmentio/kafka-go"
+)
+
+func RunWriter(addr string) {
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers:  []string{addr},
+		Topic:    "topic-A",
+		Balancer: &kafka.Hash{},
+	})
+
+	e := w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte("Hello World!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-B"),
+			Value: []byte("One!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-C"),
+			Value: []byte("Two!"),
+		},
+	)
+	color.Yellow("writer writes error: %v", e)
+
+	e = w.Close()
+	color.Yellow("writer close error: %v", e)
+
+}
